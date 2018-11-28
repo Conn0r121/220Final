42389ca6d639e26f7f68129b811f53822bfdd5af
Daniel McCaffrey <dmccaffrey@d25vw0elj1g9.campus.ithaca.lan>
2018-11-27 19:28:03 -0500

    Check README for additions

---
+++ Song.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Daniel McCaffrey on 11/27/18.
+//
+
---
+++ Song.h
@@ -0,0 +1,8 @@
+//
+// Created by Daniel McCaffrey on 11/27/18.
+//
+
+#ifndef INC_220FINAL_SONG_H
+#define INC_220FINAL_SONG_H
+
+#endif //INC_220FINAL_SONG_H
