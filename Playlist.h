7d92ae2082f6b6c2e30418a6796f965b95db1a7c
@DCaff14 <dmccaffrey@ithaca.edu>
2018-12-01 13:28:32 -0500

    shit

---
+++ Playlist.h
@@ -0,0 +1,24 @@
+//
+// Created by student on 12/1/2018.
+//
+
+#ifndef INC_220FINAL_PLAYLIST_H
+#define INC_220FINAL_PLAYLIST_H
+
+#include "Song.h"
+#include "List.h"
+
+class Playlist {
+private:
+    List* songs;
+    int duration;
+
+public:
+    virtual std::string toString() =0:
+    virtual void calcDuration() =0;
+    virtual std::string playNext() =0;
+    virtual bool isEmpty() =0;
+    virtual void addSong() =0;
+    virtual void removeSong() =0;
+};
+#endif //INC_220FINAL_PLAYLIST_H
