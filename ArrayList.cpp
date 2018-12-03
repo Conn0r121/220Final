fc35aff59519c092f2724c97c2815848854923e7
Petrepage <jpage@ithaca.edu>
2018-12-03 18:33:29 -0500

    :wq

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -10,36 +10,38 @@
 int numLinesRun=0;
 template <class T>
 void ArrayList<T>::doubleCapacity() {
-    int* newArray=::copyArray(this->array,currCapacity*2,numLinesRun);
+    T* newArray=::copyArray(this->array,currCapacity*2,numLinesRun);
     this->currCapacity=currCapacity*2;
     delete [] this->array;
     this->array = newArray;
 }
-
-ArrayList::ArrayList(int initialCapacity) {
+template <class T>
+ArrayList<T>::ArrayList(int initialCapacity) {
     if(initialCapacity<1){
         throw std::invalid_argument( "Size Must be Greater than 0" );
     }
     else {
         this->currItemCount = 0;
         this->currCapacity = initialCapacity;
-        this->array = new int[initialCapacity];
+        this->array = new T[initialCapacity];
     }
 }
-ArrayList::ArrayList(const ArrayList& arrayListToCopy) {
+template <class T>
+ArrayList<T>::ArrayList(const ArrayList& arrayListToCopy) {
     this->currItemCount=arrayListToCopy.currItemCount;
     this->currCapacity=arrayListToCopy.currCapacity;
-    this->array= new int[arrayListToCopy.currCapacity];
+    this->array= new T[arrayListToCopy.currCapacity];
     for(int i=0;i<currItemCount;i++){
         this->array[i]=arrayListToCopy.array[i];
     }
 }
-ArrayList& ArrayList::operator=(const ArrayList& arrayListToCopy){
+template <class T>
+ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T>& arrayListToCopy){
     if(this!=&arrayListToCopy) {
         delete[] this->array;
         this->array = nullptr;
         this->currCapacity = arrayListToCopy.currCapacity;
-        this->array = new int[arrayListToCopy.currCapacity];
+        this->array = new T[arrayListToCopy.currCapacity];
         for (int i = 0; i < currItemCount; i++) {
             this->array[i] = arrayListToCopy.array[i];
         }
@@ -48,16 +50,16 @@ ArrayList& ArrayList::operator=(const ArrayList& arrayListToCopy){
 
     return *this;
 }
-
-ArrayList::~ArrayList() {
+template <class T>
+ArrayList<T>::~ArrayList() {
     delete [] this->array;
     this->array = nullptr;
 }
-
+template <class T>
 void ArrayList::insertAtEnd(int itemToAdd){
     insertAt(itemToAdd,currItemCount);
 }
-
+template <class T>
 int ArrayList::getValueAt(int index){
     if(index>=currItemCount || index<0){
         throw std::out_of_range( "Out of Range" );
@@ -66,10 +68,11 @@ int ArrayList::getValueAt(int index){
         return array[index];
     }
 }
-
+template <class T>
 std::string ArrayList::toString(){
     return ::toString(array,currItemCount);
 }
+template <class T>
 bool ArrayList::isEmpty(){
     if(currItemCount==0){
         return true;
@@ -78,18 +81,23 @@ bool ArrayList::isEmpty(){
         return false;
     }
 }
+template <class T>
 int ArrayList::itemCount(){
     return currItemCount;
 }
+template <class T>
 void ArrayList::clearList(){
     currItemCount=0;
 }
+template <class T>
 int ArrayList::find(int numToFind){
     return ::find(array,currItemCount,numToFind,numLinesRun);
 }
+template <class T>
 int ArrayList::findLast(int numToFind){
     return ::findLast(array,currItemCount,numToFind,numLinesRun);
 }
+template <class T>
 int ArrayList::findMaxIndex(){
     if(currItemCount<1){
         throw std::out_of_range("Arrays must be greater than 0");
@@ -98,9 +106,11 @@ int ArrayList::findMaxIndex(){
         return ::findMaxIndex(array, currItemCount, numLinesRun);
     }
 }
+template <class T>
 void ArrayList::insertAtFront(int itemToAdd){
     insertAt(itemToAdd,0);
 }
+template <class T>
 void ArrayList::insertAt(int itemToAdd, int index){
     if(index<0 || index>currItemCount){
         throw std::out_of_range( "Out of Range" );
@@ -123,12 +133,15 @@ void ArrayList::insertAt(int itemToAdd, int index){
         }
     }
 }
+template <class T>
 int ArrayList::removeValueAtEnd(){
     return removeValueAt(currItemCount-1);
 }
+template <class T>
 int ArrayList::removeValueAtFront(){
     return removeValueAt(0);
 }
+template <class T>
 int ArrayList::removeValueAt(int index){
     if(currItemCount<1){
         throw std::out_of_range("Cannot remove anything from an empty list");
