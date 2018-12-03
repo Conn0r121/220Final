75ada7d42abbc61acfc356543955363931c0aa02
Connor Robinson <connorrobinson121@gmail.com>
2018-12-02 20:26:05 -0500

    progress towards redoing all the lists

---
+++ PlaylistList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by conno on 12/2/2018.
+//
+
+#include "PlaylistList.h"
---
+++ PlaylistList.h
@@ -0,0 +1,25 @@
+//
+// Created by conno on 12/2/2018.
+//
+
+#ifndef INC_220FINAL_PLAYLISTLIST_H
+#define INC_220FINAL_PLAYLISTLIST_H
+
+#include "Playlist.h"
+
+
+class PlaylistList {
+private:
+    Playlist* playlistList;
+    int listSize;
+    int playlistCount;
+    void doubleCapacity();
+
+public:
+    void addPlaylist(Playlist playlistToAdd);
+    void removePlaylist(std::string name);
+    Playlist getPlaylist(std::string name )
+};
+
+
+#endif //INC_220FINAL_PLAYLISTLIST_H
